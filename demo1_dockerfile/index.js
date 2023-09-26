bc0271682d399e3eb23c3871db45b424d2426bc9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-26 11:41:22 +0700

    1234

--- demo1_dockerfile/index.js
+++ demo1_dockerfile/index.js
@@ -1,6 +1,4 @@
+//  docker run -it node node --version 
+
 // docker run -v ./:/app --rm --name nodeapp -it node node /app/index.js
 console.log("Hey");
-
-const main = async () => {
-  console.log("Running..");
-};
